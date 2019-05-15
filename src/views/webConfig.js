4d09a639927aff2ff04071ddddb208bf19391453
MaZJ <miko117123@gmail.com>
2019-05-15 23:19:41 +0800

    增加配置，分离koa文件

--- src/views/webConfig.js
+++ src/views/webConfig.js
@@ -1,22 +1,22 @@
 let api;
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // 生产环境api接口地址
   api = {
-    apiURL: '/',
-    publicPath: '/',
-    apiPath: '/',
-    staticPath: '/',
-  }
-} else if (process.env.NODE_ENV === 'development') {
+    apiURL: "/",
+    publicPath: "/",
+    apiPath: "https://www.micahzj.com/",
+    staticPath: "/"
+  };
+} else if (process.env.NODE_ENV === "development") {
   // 开发环境
   api = {
-    apiURL: '/',
-    publicPath: '/',
-    apiPath: 'http://localhost:3000',
-    staticPath: '/',
-  }
+    apiURL: "/",
+    publicPath: "/",
+    apiPath: "http://localhost:3000",
+    staticPath: "/"
+  };
 }
 
 const config = {
@@ -26,4 +26,4 @@ const config = {
   apiPath: api.apiPath
 };
 
-export default config
+export default config;
--- vue.config.js
+++ vue.config.js
@@ -9,12 +9,12 @@ module.exports = {
   // 将部署应用程序的基本URL。
   // 默认情况下，Vue CLI假设您的应用程序将部署在域的根目录下。
   // https://www.my-app.com/。如果应用程序部署在子路径上，则需要使用此选项指定子路径。例如，如果您的应用程序部署在https://www.foobar.com/my-app/，集baseUrl到'/my-app/'.
-  baseUrl: process.env.NODE_ENV === "production" ? "/online/" : "/",
-  
+  baseUrl: process.env.NODE_ENV === "production" ? "./" : "/",
+
   // outputDir: 在npm run build时 生成文件的目录 type:string, default:'dist'
 
-  // outputDir: 'dist',
-
+  outputDir: "dist",
+  assetsDir: "assets",
   // pages:{ type:Object,Default:undfind }
   /*
     构建多页面模式的应用程序.每个“页面”都应该有一个相应的JavaScript条目文件。该值应该是一
@@ -60,28 +60,39 @@ module.exports = {
         return options;
       });
   },
-  
+
   // productionSourceMap：{ type:Bollean,default:true } 生产源映射
   // 如果您不需要生产时的源映射，那么将此设置为false可以加速生产构建
   productionSourceMap: false,
   // devServer:{type:Object} 3个属性host,port,https
   // 它支持webPack-dev-server的所有选项
-
+  // css相关配置
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: true,
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {},
+    // 启用 CSS modules for all css / pre-processor files.
+    modules: false
+  },
   devServer: {
     port: 8085, // 端口号
     host: "localhost",
-    https: false, // https:{type:Boolean}
+    https: true, // https:{type:Boolean}
     open: true, //配置自动启动浏览器
+    hotOnly: true // 热更新
     // proxy: 'http://localhost:3000', // 配置跨域处理,只有一个代理
     // proxy: {
-      // "/api": {
-      //   target: "<url>",
-      //   ws: true,
-      //   changeOrigin: true
-      // },
-      // "/foo": {
-      //   target: "<other_url>"
-      // }
+    // "/api": {
+    //   target: "<url>",
+    //   ws: true,
+    //   changeOrigin: true
+    // },
+    // "/foo": {
+    //   target: "<other_url>"
+    // }
     // } // 配置多个代理
   }
 };
