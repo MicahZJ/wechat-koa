e3a95174d0b7a6883a3cc8df2e694497297fe224
MaZJ <miko117123@gmail.com>
2019-06-10 21:09:11 +0800

    增加获取用户信息接口

--- src/components/header_public/control.js
+++ src/components/header_public/control.js
@@ -1,43 +1,62 @@
 import { mapMutations, mapActions, mapGetters } from "vuex";
 
-export default{
+export default {
   data() {
     return {
-      activeIndex: '1',
-      info: {}
-    }
+      activeIndex: "1",
+      info: {},
+      userInfo: {}
+    };
   },
   mounted() {
-    this.getVuexState()
+    // this.getVuexState();
+    this.getUserInfo();
   },
   computed: {
-    ...mapGetters(["userInfo"]),  
+    // ...mapGetters(["userInfo"])
   },
   methods: {
-    ...mapActions(['loginOut']),
+    ...mapActions(["loginOut"]),
 
-    getVuexState() {
-      this.info ? this.userInfo: "" 
+    // getVuexState() {
+    //   this.info ? this.userInfo : "";
+    // },
+
+    /**
+     * 获取用户信息
+     */
+    async getUserInfo() {
+      let api = "/api/login/info";
+      let requestData = {};
+
+      let res = await this.$Http.axiosGet(api, requestData);
+      if (res.code === 200) {
+        this.userInfo = res.data.userInfo;
+      }
     },
 
     /**
      * 选择访问页面
-     * @param {*} key 
-     * @param {*} keyPath 
+     * @param {*} key
+     * @param {*} keyPath
      */
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
     },
 
+    /**
+     * 用户操作
+     * @param {*} command 1为编辑，2为登出
+     */
     handleCommand(command) {
-      console.log(typeof command)
+      console.log(typeof command);
       // this.$message('click on item ' + command);
       switch (Number(command)) {
         case 1: // 编辑
-          this.toEditInfoPage()
+          this.toEditInfoPage();
           break;
         case 2: // 登出
-          this.LoginOut()
+          this.LoginOut();
           break;
         default:
           break;
@@ -48,36 +67,37 @@ export default{
      * 登出
      */
     LoginOut() {
-      this.$confirm('登出将失去异界主角光环, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(async () => {
-        this.$message({
-          type: 'success',
-          message: '你终于还是成为了路人甲!'
+      this.$confirm("登出将失去异界主角光环, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          this.$message({
+            type: "success",
+            message: "你终于还是成为了路人甲!"
+          });
+          if (await this.loginOut())
+            this.$router.replace({
+              path: "/login"
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "阔以，你现在还是主角!"
+          });
         });
-        if (await this.loginOut())
-          this.$router.replace({
-            path: '/login'
-          })
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '阔以，你现在还是主角!'
-        });          
-      });
     },
 
     /**
      * 跳转编辑页
      */
     toEditInfoPage() {
-      this.$alert('暂时还没写', '无题', {
-        confirmButtonText: '确定',
-        callback: action => {
-        }
+      this.$alert("暂时还没写", "无题", {
+        confirmButtonText: "确定",
+        callback: action => {}
       });
     }
   }
-}
+};
--- src/components/header_public/index.vue
+++ src/components/header_public/index.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="hello">
-    <el-menu class="el-menu-wrapper" 
-             mode="horizontal" 
-             background-color="#545c64" 
-             text-color="#fff" 
-             active-text-color="#ffd04b"
-             :default-active="activeIndex"
-             @select="handleSelect">
-      <el-menu-item index="1">处理中心</el-menu-item>
+    <el-menu class="el-menu-wrapper" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+      <el-menu-item index="1">
+        <router-link to="/homePage">
+          处理中心
+        </router-link></el-menu-item>
       <el-submenu index="2">
-        <template slot="title">我的工作台</template>
+        <template slot="title">
+          我的工作台
+        </template>
         <el-menu-item index="2-1">选项1</el-menu-item>
         <el-menu-item index="2-2">选项2</el-menu-item>
         <el-menu-item index="2-3">选项3</el-menu-item>
@@ -25,10 +24,10 @@
             <el-dropdown-item command="2">登出</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-        <span class="nick-class">{{info.nick}}</span>
-    </el-header>
+        <span class="nick-class">{{ userInfo.nickName }}</span>
+      </el-header>
     </el-menu>
-    <br>
+    <br />
   </div>
 </template>
 
