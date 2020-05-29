c4774ce58fcd646563e66f2114ad7e65443c0609
mikoMa <miko117123@gmail.com>
2020-05-29 20:35:26 +0800

    vue.config  

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
   // 将部署应用程序的基本URL。
   // 默认情况下，Vue CLI假设您的应用程序将部署在域的根目录下。
   // https://www.my-app.com/。如果应用程序部署在子路径上，则需要使用此选项指定子路径。例如，如果您的应用程序部署在https://www.foobar.com/my-app/，集baseUrl到'/my-app/'.
-  baseUrl: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
 
   // outputDir: 在npm run build时 生成文件的目录 type:string, default:'dist'
 
